14ce8fa120af26ec9839f633fe683f1bd466583c
Socheat Sok <socheatsok78@gmail.com>
2025-03-17 17:52:40 +0700

    Update sentrytunnel.go

--- sentrytunnel.go
+++ sentrytunnel.go
@@ -99,7 +99,7 @@ func Run() error {
 			&cli.StringFlag{
 				Name:        "dsn",
 				Usage:       "The Sentry DSN for monitoring the tunnel",
-				Sources:     cli.EnvVars("SENTRYTUNNEL_DSN"),
+				Sources:     cli.EnvVars("SENTRY_DSN"),
 				Destination: &sentrytunnel.DSN,
 				Validator: func(s string) error {
 					_, err := sentry.NewDsn(s)
@@ -109,7 +109,7 @@ func Run() error {
 			&cli.FloatFlag{
 				Name:        "trace-sample-rate",
 				Usage:       "The Sentry tunnel sample rate for sampling traces in the range [0.0, 1.0]",
-				Sources:     cli.EnvVars("SENTRYTUNNEL_TRACE_SAMPLE_RATE"),
+				Sources:     cli.EnvVars("SENTRY_TRACE_SAMPLE_RATE"),
 				Value:       1.0,
 				Destination: &sentrytunnel.TracesSampleRate,
 			},
